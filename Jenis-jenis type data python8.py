77963ea755baf90190b0f32053c3b19c4d56cc3f
rezaanastasya <rezaanastasya3@gmail.com>
2024-10-29 06:22:45 -0700

    Add files via upload

---
+++ Jenis-jenis type data python8.py
@@ -0,0 +1,35 @@
+#Tipe Data String
+foo = "Belajar Python di Duniait"
+print(foo)
+ 
+#Tipe Data Integer
+foo = 1500
+print(foo)
+ 
+#Tipe Data Float
+foo = 99.123
+print(foo)
+ 
+#Tipe Data Complex Number
+foo = 4j
+print(foo)
+ 
+#Tipe Data Boolean
+foo = True
+print(foo)
+ 
+#Tipe Data List
+foo = ["satu","dua","tiga","satu"]
+print(foo)
+ 
+#Tipe Data Tuple
+foo = ("satu","dua","tiga","satu")
+print(foo)
+ 
+#Tipe Data Set
+foo = {"satu","dua","tiga","empat"}
+print(foo)
+ 
+#Tipe Data Dictionary
+foo = {"satu":1, "dua":2.13, "tiga":"a", "empat": True}
+print(foo)
